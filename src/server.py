c6299ecab66d0927e63bb093aeca36197f2ab58e
Maciejlys <maciej.lyszczarz98@gmail.com>
2022-01-06 16:32:24 +0100

    deployment

--- src/server.py
+++ src/server.py
@@ -4,8 +4,6 @@ import jwt
 import datetime
 import os
 
-port = int(os.environ.get('PORT', 5000))
-
 app = Flask(__name__)
 
 users = {}
@@ -96,4 +94,5 @@ def getUsers():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=port)
+    port = os.environ.get('PORT', 5000)
+    app.run(debug=False, host='0.0.0.0', port=port)
