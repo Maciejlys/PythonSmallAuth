6a3ded5d13c4ec5980ce33d19c9d50b576b65b78
Maciejlys <maciej.lyszczarz98@gmail.com>
2022-01-19 17:31:21 +0100

    database added

--- src/server.py
+++ src/server.py
@@ -1,17 +1,49 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS, cross_origin
+from flask_sqlalchemy import SQLAlchemy
 import hashlib
 import jwt
-import datetime
+from datetime import datetime
+import os
+from dotenv import load_dotenv
 import os
 
+load_dotenv('./.env')
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-users = {}
-usersTokens = {}
+db = SQLAlchemy(app)
+
+
+class Users(db.Model):
+    __tablename__ = 'Users'
+    id = db.Column(db.Integer, primary_key=True)
+    userName = db.Column(db.String(200))
+    password = db.Column(db.String(200))
+    createdAt = db.Column(db.String(200))
+
+    def __init__(self, userName, password):
+        time = datetime.today()
+        self.userName = userName
+        self.password = password
+        self.createdAt = str(str(time.day) + "-" +
+                             str(time.month) + "-" + str(time.year))
+
+
+class ValidTokens(db.Model):
+    __tablename__ = 'Tokens'
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer)
+    token = db.Column(db.String(200))
+
+    def __init__(self, userId, token):
+        self.userId = userId
+        self.token = token
+
 
 SECRET = 'superSecret'
 
@@ -19,7 +51,7 @@ SECRET = 'superSecret'
 def validateRegisterBody(body):
     if(body is None):
         raise Exception("Invalid body")
-    if(body["userName"] in users):
+    if db.session.query(Users).filter(Users.userName == body["userName"]).count() != 0:
         raise Exception("This user name is already taken")
 
 
@@ -28,7 +60,11 @@ def hashPassword(password):
 
 
 def registerUser(user):
-    users[user["userName"]] = hashPassword(user["password"])
+    password = hashPassword(user["password"])
+    userName = user["userName"]
+    data = Users(userName, password)
+    db.session.add(data)
+    db.session.commit()
 
 
 @app.route('/register', methods=['POST'])
@@ -46,20 +82,27 @@ def register():
 def validateLoginBody(body):
     if(body is None):
         raise Exception("Invalid body")
-    if not userExists(body["userName"]):
-        raise Exception("Invalid username or password")
-
-
-def userExists(userName):
-    return userName in users
+    if db.session.query(Users).filter(Users.userName == body["userName"]).count() == 0:
+        raise Exception("This username doesn't exist")
 
 
 def loginUser(user):
-    validPassoword = users[user["userName"]]
-    if validPassoword == hashPassword(user["password"]):
+    password = Users.query.filter_by(
+        userName=user['userName']).first().password
+    id = Users.query.filter_by(
+        userName=user['userName']).first().id
+    if password == hashPassword(user["password"]):
         token = jwt.encode(
-            {"user": user["userName"], "iat": datetime.datetime.now()}, SECRET)
-        usersTokens[user["userName"]] = token
+            {"user": user["userName"], "iat": datetime.now()}, SECRET)
+        if db.session.query(ValidTokens).filter(ValidTokens.userId == id).count() != 0:
+            obj = db.session.query(ValidTokens).filter(
+                ValidTokens.userId == id).first()
+            db.session.delete(obj)
+            db.session.commit()
+
+        data = ValidTokens(id, token)
+        db.session.add(data)
+        db.session.commit()
         return make_response({"token": token}), 200
 
     else:
@@ -82,11 +125,9 @@ def login():
 def verify():
     token = request.headers['token']
     try:
-        decoded = jwt.decode(token, SECRET, 'HS256')
-        currentToken = usersTokens[decoded["user"]]
-        if(token != currentToken):
-            return make_response("invalid token"), 400
-        return make_response(), 200
+        if db.session.query(ValidTokens).filter(ValidTokens.token == token).count() == 1:
+            return make_response(), 200
+        return make_response("invalid token"), 400
     except Exception as err:
         return make_response("unathorized"), 400
 
@@ -95,8 +136,9 @@ def verify():
 @cross_origin()
 def getUsers():
     userNames = []
-    for key in users.keys():
-        userNames.append(key)
+    users = Users.query.all()
+    for user in users:
+        userNames.append([user.userName, user.createdAt])
     return make_response(jsonify(userNames)), 200
 
 
